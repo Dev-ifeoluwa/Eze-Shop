198705cf376129102a2ff79e406d449a70ecc0b1
Your Name <youremail@example.com>
2025-08-20 20:36:14 +0100

    Fix

--- my-app/component/Engaged.tsx
+++ my-app/component/Engaged.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Engaged = () => {
   return (
     <div className="md:flex flex-col py-1 mx-6 lg:flex flex-row">
-      <div className="relative h-[35vh] w-full border-rounded cursor-pointer md:h-[55vh]">
+      <div className="relative h-[25vh] w-full border-rounded cursor-pointer md:h-[55vh]">
         {/* Background image */}
             <Image
               src="/img5.jpg"
@@ -15,7 +15,7 @@ const Engaged = () => {
               priority
             />
         {/* text */}
-        <div className="flex flex-col items-center justify-center gap-2 pt-18 mr-50 md:mt-10">
+        <div className="flex flex-col items-center justify-center gap-2 pt-10 mr-50 md:mt-10">
           <p className="text-md text-gray-400">Need Gift?</p>
           <h1 className="text-xl font-semibold md:text-4xl">Gift Guide</h1>
         </div>
@@ -24,7 +24,7 @@ const Engaged = () => {
 
 
 
-      <div className="relative h-[35vh] w-full border-rounded cursor-pointer md:h-[55vh]">
+      <div className="relative h-[25vh] w-full border-rounded cursor-pointer md:h-[55vh]">
         {/* Background image */}
         <Image
           src="/img6.jpg"
